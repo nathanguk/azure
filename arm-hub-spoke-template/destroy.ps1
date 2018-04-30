78b32a131eef72b534a6c39c0706d54382deaf44
nathanguk <nathan.gaskill@ansgroup.co.uk>
2018-04-30 14:31:39 +0100

    updated

--- arm-hub-spoke-template/destroy.ps1
+++ arm-hub-spoke-template/destroy.ps1
@@ -7,15 +7,21 @@ Write-Output "Logged into Azure RM account"
 $parametersUri="https://raw.githubusercontent.com/nathanguk/azure/master/arm-hub-spoke-template/azuredeploy.parameters.json"
 $parameters = Invoke-RestMethod -Uri $parametersUri
 
+# Deletev the policies
+Remove-AzureRmPolicyAssignment -Name "Allowed virtual machine SKUs" -Scope "/subscriptions/$((Get-AzureRmContext).Subscription.Id)" 
+Remove-AzureRmPolicyAssignment -Name "Apply default tags and default values" -Scope "/subscriptions/$((Get-AzureRmContext).Subscription.Id)" 
+Remove-AzureRmPolicyAssignment -Name "Allowed Locations" -Scope "/subscriptions/$((Get-AzureRmContext).Subscription.Id)" 
+Remove-AzureRmPolicySetDefinition -Name "TagInitiative" -Force
 
-# Deploy the Hub resource group
+
+# Delete the Hub resource group
 $hubrg = $parameters.parameters.hub.value.resourceGroup
 Write-Output "Deleting Resource Locks: "$hubrg
 Get-AzureRmResourceLock -ResourceGroupName $hubrg | Remove-AzureRmResourceLock -Force
 Write-Output "Deleting Resource Group: "$hubrg
 Remove-AzureRmResourceGroup -Name $hubrg -Force
 
-# Deploy the Spoke resource group/s
+# Delete the Spoke resource group/s
 foreach($spoke in $parameters.parameters.spokes.value){
     $rg = $spoke.resourceGroup
     Write-Output "Deleting Resource Locks: "$rg
